e47b687254bdf89abbd83dc5d161c568bf8b56a2
Luis-smp <luisotaviocerqueirasampaio@gmail.com>
2025-08-26 20:29:32 -0300

    Formulario_Banco
    Desenvolvimento do banco de dados e formulario.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//Importar o controller
+require_once('./controllers/clientesController.php');
+//Instância e execução do método construct
+$controller = new ClientesController();
+//Execução da sequência de métodos que fará o SELECT
+$controller->Listar();
+//Execução da sequência de métodos que fará o Insert
+ $controller->insert();
+//Execução da sequência de métodos que fará o UPDATE
+ $controller->Atualizar();
+
+ $controller->Deletar();
+
+?>
